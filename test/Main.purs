a3f518e37164ae44e5942805f77de83abe3b7004
AHaliq <liqo12@hotmail.com>
2021-09-13 14:50:11 +0800

    import from Index.purs from Main test purs

--- test/Main.purs
+++ test/Main.purs
@@ -4,8 +4,10 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Class.Console (log)
+import Index (str)
 
 main :: Effect Unit
 main = do
   log "🍝"
+  log str
   log "You should add some tests."
