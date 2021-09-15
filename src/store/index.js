8c5da7b16c5d02145b7b4a86fff8c3432afaedf4
AHaliq <liqo12@hotmail.com>
2021-09-15 09:22:03 +0800

    update initial console output

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import { str } from '../index.purs';
 export default createStore({
   state: {
     pur_str: str,
-    editor_str: 'press eval input to calc.jison grammar',
+    editor_str: 'press eval to parse input according to calc.jison grammar',
   },
   getters: {
     getField,
