dc6f1eaeb259ef27d4cfe59616e6b8f38a5015c4
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2021-09-21 16:43:30 +0800

    add question box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template lang='pug'>
-div(class='home')
-  img(alt='Vue logo' src='../assets/logo.png')
-  HelloWorld(msg='Welcome to Your Vue.js App')
-  h1(class='title') {{pur_str}}
-  button(class='button is-large') Bulma button
+.columns
+  .column
+    img(alt='Vue logo' src='../assets/logo.png')
+    HelloWorld(msg='Welcome to Your Vue.js App')
+    h1.title {{pur_str}}
+    button.button.is-large Bulma button
+    Question
 </template>
 
 <script>
 // @ is an alias to /src
 import { mapFields } from 'vuex-map-fields';
 import HelloWorld from '@/components/HelloWorld.vue';
+import Question from '@/components/Question.vue';
 
 export default {
   name: 'Home',
@@ -18,6 +21,7 @@ export default {
   },
   components: {
     HelloWorld,
+    Question,
   },
 };
 </script>
