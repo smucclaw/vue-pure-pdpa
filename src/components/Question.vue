5f41555e86b5e81190b07865dca391af4d8a7279
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2021-09-21 16:44:38 +0800

    add additional support

---
+++ src/components/Question.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.box.has-text-left-tablet
+  h1.title Are buttons allowed?
+  .buttons
+    button.button.has-text-left-tablet.is-fullwidth.question
+      font-awesome-icon(icon="check-square")
+      |
+      span Lorem ipsum dolor sit amet
+    button.button.has-text-left-tablet.is-fullwidth.question
+      font-awesome-icon(icon='check-square')
+      span Lorem ipsum dolor sit amet
+</template>
+
+<style lang="scss">
+.question {
+  justify-content: flex-start !important;
+}
+</style>
---
+++ src/utils/fontawesome.js
@@ -0,0 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCheckSquare);
+
+export default FontAwesomeIcon;
