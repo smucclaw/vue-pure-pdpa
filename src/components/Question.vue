e6ee84cfb01031c700692f11c4dc667e68ce8529
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2021-09-22 17:55:36 +0800

    use checkbox instead of button

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,12 +1,22 @@
 <template lang="pug">
 .box.has-text-left-tablet
   h1.title {{ title }}
-  .buttons
-    Button(v-for='choice in choices', :text='choice')
+  //-.buttons
+    Button(
+      v-for='choice in choices',
+      :text='choice',
+      @selected='getSelected'
+    )
+  .field
+    Checkbox(
+      v-for='choice in choices',
+      :text='choice'
+    )
 </template>
 
 <script>
 import Button from '@/components/Button.vue';
+import Checkbox from '@/components/form/Checkbox.vue';
 
 export default {
   name: 'Question',
@@ -16,12 +26,23 @@ export default {
   },
   components: {
     Button,
+    Checkbox,
+  },
+  data() {
+    return {
+      isSelected: false,
+    };
+  },
+  methods: {
+    getSelected(e) {
+      this.isSelected = !this.isSelected;
+      console.log(e, this.isSelected);
+    },
+  },
+  computed: {
   },
 };
 </script>
 
 <style lang="scss">
-.question {
-  justify-content: flex-start !important;
-}
 </style>
---
+++ src/components/form/Checkbox.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.control
+  label.is-check.is-fullwidth.is-justify-content-flex-start
+    input(type='checkbox', v-model='checked')
+    font-awesome-icon.icon.checkmark(icon='check')
+    span {{ text }}
+</template>
+
+<script>
+export default {
+  name: 'Checkbox',
+  props: {
+    text: String,
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
