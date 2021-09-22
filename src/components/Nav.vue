602d86a8b338b82ac02412f03c234f5fa4382432
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2021-09-22 22:30:02 +0800

    add show/hide to nav burger

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -6,14 +6,39 @@ nav.navbar.is-dark
       a.navbar-burger(
         role='button',
         aria-label='menu',
-        aria-expanded='false'
+        aria-expanded='false',
+        @click='toggleNav',
+        :class='showNav'
       )
         span(aria-hidden='true')
         span(aria-hidden='true')
         span(aria-hidden='true')
-    .navbar-menu
+    .navbar-menu(
+      @click='toggleNav',
+      :class='showNav'
+    )
       .navbar-end
         router-link.navbar-item(to='/about') About
         router-link.navbar-item(to='/questions') Questions
         router-link.navbar-item(to='/editor') Editor
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isNavActive: false,
+    };
+  },
+  methods: {
+    toggleNav() {
+      this.isNavActive = !this.isNavActive;
+    },
+  },
+  computed: {
+    showNav() {
+      return this.isNavActive ? 'is-active' : '';
+    },
+  },
+};
+</script>
