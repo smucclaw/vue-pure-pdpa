7df216f877d791afd494080382875321cda86f17
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2022-01-12 14:43:25 +0800

    change all routes to lazy load

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
-import Questions from '../views/Questions.vue';
-import Editor from '../views/Editor.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: () => import('@/views/Home.vue'),
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component() {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue');
-    },
-  },
-  {
-    path: '/questions',
-    name: 'Questions',
-    component: Questions,
+    component: () => import('@/views/About.vue'),
   },
   {
     path: '/editor',
     name: 'Editor',
-    component: Editor,
+    component: () => import('@/views/Editor.vue'),
   },
 ];
 
