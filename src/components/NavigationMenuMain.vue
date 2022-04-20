a293cdedd3b48ae31e105a759ff09552d6720c6e
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2022-04-20 14:47:40 +0800

    show only when slots active

--- src/components/NavigationMenuMain.vue
+++ src/components/NavigationMenuMain.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="navbar-menu" :class="showNav">
-    <div class="navbar-start">
+    <div class="navbar-start" v-if="$slots.start">
       <slot name="start"></slot>
     </div>
-    <div class="navbar-end">
+    <div class="navbar-end" v-if="$slots.end">
       <slot name="end"></slot>
     </div>
   </div>
