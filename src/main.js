f829e2a7618291229718d16d21c443c9be1e4b52
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2022-04-25 15:48:36 +0800

    remove fontawesome util script

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
-import FontAwesomeIcon from '@/utils/fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -7,7 +6,6 @@ import store from './store';
 require('@/assets/main.scss');
 
 createApp(App)
-  .component('font-awesome-icon', FontAwesomeIcon)
   .use(store)
   .use(router)
   .mount('#app');
