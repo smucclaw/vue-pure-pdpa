4797602adf4d44277d0f482ac7ee21a939ca01a3
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2022-04-28 09:39:07 +0800

    nest buttons in div with control class

--- src/components/QuestionRadio.vue
+++ src/components/QuestionRadio.vue
@@ -1,22 +1,24 @@
 <template>
-  <label
-    class="button is-radio-button px-2 mr-2"
-    v-for="(opt, index) in btnOptions"
-    :key="index"
-    :class="getColor(opt.color, opt.value, selectedValue)"
-    :checked="opt.value === selectedValue"
-    >
-    <input
-      type="radio"
-      v-model="selectedValue"
-      :id="opt.value"
-      :value="opt.value"
-      />
-    <FontAwesomeIcon
-      class="icon is-small mx-0"
-      :icon="opt.icon"
-      />
-  </label>
+  <div class="control">
+    <label
+      class="button is-radio-button px-2"
+      v-for="(opt, index) in btnOptions"
+      :key="index"
+      :class="getColor(opt.color, opt.value, selectedValue)"
+      :checked="opt.value === selectedValue"
+      >
+      <input
+        type="radio"
+        v-model="selectedValue"
+        :id="opt.value"
+        :value="opt.value"
+        />
+      <FontAwesomeIcon
+        class="icon is-small mx-0"
+        :icon="opt.icon"
+        />
+    </label>
+  </div>
 </template>
 
 <script>
@@ -79,3 +81,13 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.control > label {
+  margin-right: 0.5rem !important;
+}
+
+.control > label:last-child {
+  margin-right: 0 !important;
+}
+</style>
