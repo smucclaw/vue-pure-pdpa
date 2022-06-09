11e7e1fa08fc2ad9e5bc36ec409a09af1d26cf2c
Teo Wei Shen <7098637+tws4793@users.noreply.github.com>
2022-06-09 09:56:24 +0800

    initialise unit tests

---
+++ tests/unit/BaseNavigation.spec.js
@@ -0,0 +1,17 @@
+import { shallowMount } from '@vue/test-utils';
+import BaseNavigation from '@/components/BaseNavigation.vue';
+
+const factory = (propsData) => shallowMount(BaseNavigation, {
+  ...propsData,
+});
+
+describe('', () => {
+  it('renders correctly', () => {
+    expect(true).toBe(true);
+  });
+
+  it('test 2', () => {
+    const wrapper = factory({
+    });
+  });
+});
