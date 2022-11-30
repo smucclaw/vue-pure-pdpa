54bcf84a0756827d65f368b760b36b4fbdecb34c
Max Loo <maxloo@smu.edu.sg>
2022-11-30 17:26:54 +0800

    implemented test version of multiple heads feature

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -5,7 +5,11 @@
     <p>ccc</p>
   </div> -->
   <TheQuestion v-slot="slotProps">
+<<<<<<< HEAD
+    <div class="columns">
+=======
     <!-- <div class="columns">
+>>>>>>> 60e95d6... implemented a test version of the multiple heads feature
       <div class="column is-one-fifth has-text-left is-size-4">
         <div class="vertical-container">Assessment</div>
         <div class="vertical-container">Notify Individuals Notify PDPC</div>
@@ -14,8 +18,12 @@
       <div class="column has-text-left is-size-6">
         <QuestionBase :question="slotProps.questions" />
       </div>
+<<<<<<< HEAD
+    </div>
+=======
     </div> -->
     <QuestionBase :question="slotProps.questions" />
+>>>>>>> 60e95d6... implemented a test version of the multiple heads feature
   </TheQuestion>
   <!-- <div class="vertical-container">
     <p>aaa</p>
