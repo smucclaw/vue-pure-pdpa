19cf0d284182e8fbf13c434c9bf657a990177161
Wong Meng Weng <mwwong@smu.edu.sg>
2023-05-28 12:49:33 +0800

    starting work on integrating ladder diagrams into Vue

---
+++ src/components/LadderDiagram.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="ladderTest1" style="font-size:1.5em;">Future home of the Ladder diagrame</div>
+</template>
+
+<script setup>
+import { BoolVar, AllQuantifier, AnyQuantifier, LadderDiagram } from 'ladder-diagram';
+
+const props = defineProps({question: Object})
+
+</script>
+
+<style module>
+@import '~ladder-diagram/css/ladder-sides.css';
+</style>
---
+++ src/views/Ladder.vue
@@ -0,0 +1,21 @@
+<template>
+  <TheQuestion v-slot="slotProps">
+    <LadderDiagram
+      class="block"
+      :question="slotProps.questions"
+    />
+  </TheQuestion>
+</template>
+
+<script>
+import TheQuestion from '@/components/TheQuestion.vue';
+import LadderDiagram from '@/components/LadderDiagram.vue';
+
+export default {
+  name: 'Ladder',
+  components: {
+    TheQuestion,
+    QuestionDiagram,
+  },
+};
+</script>
