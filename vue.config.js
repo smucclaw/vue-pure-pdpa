dfbe8ab64ae95d9f812a20472a0a8f5df051910c
Wong Meng Weng <mwwong@smu.edu.sg>
2023-05-29 12:54:34 +0800

    uncomment https

--- vue.config.js
+++ vue.config.js
@@ -10,6 +10,17 @@ const getPublicPath = () => {
 
 module.exports = {
   publicPath: getPublicPath(),
+  devServer: {
+    server: {
+     type: 'https',
+     options: {
+     key:  '/etc/letsencrypt/live/cclaw.legalese.com/privkey.pem',
+     cert: '/etc/letsencrypt/live/cclaw.legalese.com/cert.pem',
+     },
+    },
+    allowedHosts: "all",
+    historyApiFallback: true,
+  },
   lintOnSave: false,
   chainWebpack: (config) => {
     // Purescript Loader
