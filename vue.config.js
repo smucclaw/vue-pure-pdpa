ae7d03f94626e6440bc4168af038b0901ca0a21a
Max Loo <maxloo@smu.edu.sg>
2023-06-06 18:59:21 +0800

    trying to change dolora5 to fit main

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,7 @@ const getPublicPath = () => {
 module.exports = {
   publicPath: getPublicPath(),
   lintOnSave: false,
+  // the HTTPS logic is farther down in this file
   chainWebpack: (config) => {
     // Purescript Loader
     config.module
@@ -21,13 +22,40 @@ module.exports = {
       .options({
         spago: true,
       });
+
     config.module
       .rule('jison')
       .test(/\.jison$/)
-      .use('jison-gho-loader')
-      .loader('jison-gho-loader');
+          .use('jison-gho-loader')
+          .loader('jison-gho-loader');
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        options['compilerOptions'] = { isCustomElement: (e) => ["clippath"].includes(e) }
+        return options
+      })
+
   },
 }
+
+// On the shared cclaw server, when v8k brings up vue instances on
+// port 8xxx, we choose to HTTPS-enable those instances. Why? For good
+// form; since about 2011, people have shouted "HTTPS Everywhere!" and
+// if they found us serving the web apps without HTTPS, even in
+// development mode, those people would get Internet-angry.
+//
+// So we set CCLAW_HTTPS in gunicorn.conf.py, and also in
+// /etc/zsh/zshenv to be on the safe side. Why? In case vue gets `npm
+// run serve`d outside of the gunicorn context; maybe somebody is
+// running a vue instance by hand just to debug, and they want to
+// simulate conditions as similar as possible to the "live" system.
+
+// However, on our local servers, when we are doing dev on our laptops
+// or whatever, we don't have SSL certs for our localhosts, so we turn
+// off the HTTPS bits and expect to hit http://localhost:8888 or
+// whatever. If you're on your laptop, do not set this environment variable.
 
 if (process.env.CCLAW_HTTPS) {
   module.exports.devServer = {
@@ -42,3 +70,4 @@ if (process.env.CCLAW_HTTPS) {
     historyApiFallback: true,
   }
 }
+
