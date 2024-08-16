5d562aabfbef9ce2ca14b588a07b0496b7c98e8c
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-08-16 11:12:21 +0800

    Enable cypress on CI

--- tests/e2e/specs/home.cy.js
+++ tests/e2e/specs/home.cy.js
@@ -2,7 +2,7 @@ describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('http://localhost:8080/vue-pure-pdpa/');
     cy.get("[data-test='response-message']").contains(
-      "It depends..."
-    )
+      'It depends...',
+    );
   });
 });
