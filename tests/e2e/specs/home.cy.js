afc5a05c608c95be512f1fac5641293857edefd2
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-08-19 15:35:56 +0800

    Add test for Walk and Eat (#108)

--- tests/e2e/specs/home.cy.js
+++ tests/e2e/specs/home.cy.js
@@ -5,4 +5,13 @@ describe('My First Test', () => {
       'It depends...',
     );
   });
+
+  it('Press <<does the person walk?>>', () => {
+    cy.visit('http://localhost:8080/vue-pure-pdpa/');
+    cy.get("[data-test='does the person walk?']").find('label').first().click();
+    cy.get("[data-test='does the person eat?']").find('label').first().click();
+    cy.get("[data-test='response-message']").contains(
+      'Yes!',
+    );
+  });
 });
