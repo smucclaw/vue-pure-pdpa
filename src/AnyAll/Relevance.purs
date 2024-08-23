bb65c5468b5962ed812048d80161d5db78e64e87
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-08-23 09:22:49 +0800

    Clean AnyAll, split out relevantHard

--- src/AnyAll/Relevance.purs
+++ src/AnyAll/Relevance.purs
@@ -47,18 +47,52 @@ relevant sh dp marking parentValue nl self =
           longtext = fromMaybe "" (Map.lookup word lgDict)
         pure $ Tuple lg longtext
 
-  getChildren (Leaf _) = []
-  getChildren (Not x) = getChildren x
-  getChildren (Any _ c) = c
-  getChildren (All _ c) = c
 
-  ask2hide :: Q -> Q
-  ask2hide (Q q@{ shouldView: Ask }) = Q $ q { shouldView = Hide }
-  ask2hide x = x
+getChildren (Leaf _) = []
+getChildren (Not x) = getChildren x
+getChildren (Any _ c) = c
+getChildren (All _ c) = c
 
-  ask2view :: ShouldView -> ShouldView
-  ask2view Ask = View
-  ask2view x = x
+ask2hide :: Q -> Q
+ask2hide (Q q@{ shouldView: Ask }) = Q $ q { shouldView = Hide }
+ask2hide x = x
+
+ask2view :: ShouldView -> ShouldView
+ask2view Ask = View
+ask2view x = x
+
+relevantHard :: DisplayPref -> Marking -> Maybe Bool -> NLDict -> Item String -> Q
+relevantHard dp marking parentValue nl self =
+  let
+    selfValue = evaluate Hard marking self
+    initVis =
+      if isJust parentValue then
+        if parentValue == selfValue then View
+        else Hide
+      else if isJust (evaluate Hard marking self) then View
+      else Ask
+    -- we compute the initial visibility of the subtree.
+    -- if our initial visibility is to hide, then we mute all our children by converting Ask to Hide; but if any of our children are View, we leave them as View.
+    paintedChildren = (if initVis /= Hide then identity else ask2hide) <$> relevantHard dp marking selfValue nl <$> getChildren self
+    makeQNode itemNode = case itemNode of
+      Leaf x -> mkQ (initVis) (Simply x) (nlMap x nl) Nothing (lookupMarking x marking) []
+      Not x -> makeQNode x -- [TODO] we should have a SimplyNot as well
+      Any label items -> mkQ (ask2view initVis) Or  (nlMap (label2pre label) nl) (Just label) (Default $ Left selfValue) paintedChildren
+      All label items -> mkQ (ask2view initVis) And (nlMap (label2pre label) nl) (Just label) (Default $ Left selfValue) paintedChildren
+  in -- convert to a QTree for output
+    makeQNode self
+  where
+  nlMap word nldict =
+    let
+      langs = Set.toUnfoldable $ Map.keys nldict :: Array String
+    in
+      Map.fromFoldable $ do
+        lg <- langs
+        let
+          lgDict = fromMaybe Map.empty (Map.lookup lg nl)
+          longtext = fromMaybe "" (Map.lookup word lgDict)
+        pure $ Tuple lg longtext
+
 
 -- well, it depends on what values the children have. and that depends on whether we're assessing them in soft or hard mode.
 evaluate :: Hardness -> Marking -> Item String -> Maybe Bool
