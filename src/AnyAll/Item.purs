4ce437abfed3e631992700f73987fe21a1611f9f
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-09-06 09:05:46 +0800

    Extract Item code into a separate module (#123)

---
+++ src/AnyAll/Item.purs
@@ -0,0 +1,85 @@
+module AnyAll.Item(
+  Item(..),
+  Label(..),
+  nnf,
+  decodeIt,
+  label2pre,
+  label2post
+) where
+
+import Prelude
+
+import Control.Monad.Except (runExcept)
+import Data.Either (either)
+import Foreign.Generic
+import Partial.Unsafe (unsafeCrashWith)
+
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
+
+--
+-- the "native" data type represents an And/Or structure as a simple tree of Items
+--
+
+data Item a
+  = Leaf a
+  | All (Label a) (Array (Item a))
+  | Any (Label a) (Array (Item a))
+  | Not (Item a)
+
+
+
+-- | nnf based on the nnf found in dsl/lib/haskell/anyall/src/AnyAll/BoolStruct.hs
+nnf :: forall a. Item a -> Item a
+nnf (Not (Not p)) = nnf p
+nnf (Not (All l ps)) = Any l $ nnf <$> Not <$> ps
+nnf (Not (Any l ps)) = All l $ nnf <$> Not <$> ps
+nnf (All l ps) = All l (nnf <$> ps)
+nnf (Any l ps) = Any l (nnf <$> ps)
+nnf x = x
+
+
+-- boilerplate for class derivations
+derive instance eqItem :: (Eq a) => Eq (Item a)
+derive instance genericItem :: Generic (Item a) _
+instance showItem :: (Show a) => Show (Item a) where
+  show eta = genericShow eta
+
+instance encodeItem :: (Encode a) => Encode (Item a) where
+  encode eta = genericEncode defaultOptions eta
+
+instance decodeItem :: (Decode a) => Decode (Item a) where
+  decode eta = genericDecode defaultOptions eta
+
+decodeIt :: Foreign -> (Item String)
+decodeIt f =
+  either
+    (\e -> unsafeCrashWith $ "error while decoding Item: " <> show e)
+    (\v -> v)
+    (runExcept (decode f))
+
+--
+-- Item uses Label to prefix a tree with strings like "all of the following" or "any of the below"
+--
+data Label a
+  = Pre a
+  | PrePost a a
+
+derive instance eqLabel :: (Eq a) => Eq (Label a)
+derive instance genericLabel :: Generic (Label a) _
+instance showLabel :: (Show a) => Show (Label a) where
+  show = genericShow
+
+instance encodeLabel :: (Encode a) => Encode (Label a) where
+  encode eta = genericEncode defaultOptions eta
+
+instance decodeLabel :: (Decode a) => Decode (Label a) where
+  decode eta = genericDecode defaultOptions eta
+
+label2pre ∷ Label String → String
+label2pre (Pre x) = x
+label2pre (PrePost x _) = x
+
+label2post ∷ Label String → String
+label2post (PrePost _ y) = y
+label2post (Pre _) = "" -- maybe throw an error?
--- src/AnyAll/Types.purs
+++ src/AnyAll/Types.purs
@@ -1,6 +1,5 @@
 module AnyAll.Types(
-  Item(..),
-  Label(..),
+  module AnyAll.Item,
   NLDict,
   Marking(..),
   Default(..),
@@ -13,12 +12,9 @@ module AnyAll.Types(
   Hardness(..),
   StdinSchema(..),
   ForD3(..),
-  nnf,
-  decodeIt,
   DefaultRecord,
   getMarking,
   mkQ,
-  label2pre,
   markup,
   qoutjs
 ) where
@@ -50,71 +46,8 @@ import Foreign.Generic
 import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
 
+import AnyAll.Item
 
---
--- the "native" data type represents an And/Or structure as a simple tree of Items
---
-
-data Item a
-  = Leaf a
-  | All (Label a) (Array (Item a))
-  | Any (Label a) (Array (Item a))
-  | Not (Item a)
-
-
-
--- | nnf based on the nnf found in dsl/lib/haskell/anyall/src/AnyAll/BoolStruct.hs
-nnf :: forall a. Item a -> Item a
-nnf (Not (Not p)) = nnf p
-nnf (Not (All l ps)) = Any l $ nnf <$> Not <$> ps
-nnf (Not (Any l ps)) = All l $ nnf <$> Not <$> ps
-nnf (All l ps) = All l (nnf <$> ps)
-nnf (Any l ps) = Any l (nnf <$> ps)
-nnf x = x
-
-
--- boilerplate for class derivations
-derive instance eqItem :: (Eq a) => Eq (Item a)
-derive instance genericItem :: Generic (Item a) _
-instance showItem :: (Show a) => Show (Item a) where
-  show eta = genericShow eta
-
-instance encodeItem :: (Encode a) => Encode (Item a) where
-  encode eta = genericEncode defaultOptions eta
-
-instance decodeItem :: (Decode a) => Decode (Item a) where
-  decode eta = genericDecode defaultOptions eta
-
-decodeIt :: Foreign -> (Item String)
-decodeIt f =
-  either
-    (\e -> unsafeCrashWith $ "error while decoding Item: " <> show e)
-    (\v -> v)
-    (runExcept (decode f))
-
---
--- Item uses Label to prefix a tree with strings like "all of the following" or "any of the below"
---
-data Label a
-  = Pre a
-  | PrePost a a
-
-derive instance eqLabel :: (Eq a) => Eq (Label a)
-derive instance genericLabel :: Generic (Label a) _
-instance showLabel :: (Show a) => Show (Label a) where
-  show = genericShow
-
-instance encodeLabel :: (Encode a) => Encode (Label a) where
-  encode eta = genericEncode defaultOptions eta
-
-instance decodeLabel :: (Decode a) => Decode (Label a) where
-  decode eta = genericDecode defaultOptions eta
-
-label2pre (Pre x) = x
-label2pre (PrePost x _) = x
-
-label2post (PrePost _ y) = y
-label2post (Pre _) = "" -- maybe throw an error?
 
 type NLDict = Map.Map String (Map.Map String String)
 
