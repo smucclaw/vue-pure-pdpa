211eca34b19dc0b1fd74730196143368103ee0e8
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-09-24 10:45:06 +0800

    Config for E2E

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL ?? '/vue-pure-pdpa/',
   plugins: [
     vue(),
     vueDevTools(),
