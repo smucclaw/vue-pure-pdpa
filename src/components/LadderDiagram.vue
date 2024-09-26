7d22e7dfc1249f9ff27421af5cac3e41367fcef8
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-09-26 10:53:23 +0800

    Delete unused deps and upgrade others

--- src/components/LadderDiagram.vue
+++ src/components/LadderDiagram.vue
@@ -133,30 +133,3 @@ onUpdated(() => {
 <style>
 @import 'ladder-diagram/css/ladder.css';
 </style>
-
-<!--
-  // [TODO] this will come from the store
-  const sampleData = ref(
-  new AllQuantifier([
-  new AnyQuantifier([
-  new BoolVar(    "Left  Null",    false, null, null  ),
-  new BoolVar(    "Left  Nothing", false, 'U',  null  ),
-  new BoolVar(    "Left  True",    false, 'T',  null  ),
-  new BoolVar(    "Left  False",   false, 'F',  null  ),
-  new BoolVar("Not Left  Null",    true,  null, null  ),
-  new BoolVar("Not Left  Nothing", true,  'U',  null  ),
-  new BoolVar("Not Left  False",   true,  'F',  null  ),
-  new BoolVar("Not Left  True",    true,  'T',  null  ),
-  ]),
-  new AnyQuantifier([
-  new BoolVar(    "Right Null",    false, null, null  ),
-  new BoolVar(    "Right Nothing", false, null, 'U'   ),
-  new BoolVar(    "Right True",    false, null, 'T'   ),
-  new BoolVar(    "Right False",   false, null, 'F'   ),
-  new BoolVar("Not Right Null",    true,  null, null  ),
-  new BoolVar("Not Right Nothing", true,  null, 'U'   ),
-  new BoolVar("Not Right False",   true,  null, 'F'   ),
-  new BoolVar("Not Right True",    true,  null, 'T'   ),
-  ])
-  ])
-  ) -->
