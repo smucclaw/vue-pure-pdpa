71afaa2db9116212990b93c6241d3bc2e60d6960
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-10-07 10:04:26 +0800

    Move tests to default location

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  fixturesFolder: 'tests/e2e/fixtures',
-  screenshotsFolder: 'tests/e2e/screenshots',
-  videosFolder: 'tests/e2e/videos',
   e2e: {
     supportFile: false,
-    specPattern: 'tests/e2e/specs/**/*.cy.{js,jsx,ts,tsx}',
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
   },
 })
