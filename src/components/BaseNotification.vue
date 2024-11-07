a4900987cb5fa0c5288c01d774f4d0ff91a137b8
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-11-07 15:06:18 +0800

    Convert BaseNotification to setup script (#179)

--- src/components/BaseNotification.vue
+++ src/components/BaseNotification.vue
@@ -5,26 +5,17 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Notification',
-  props: {
-    hasCloseButton: Boolean,
-    themeColor: String,
-    isLight: Boolean,
-    content: String,
-  },
-  data() {
-    return {
-    };
-  },
-  computed: {
-    isCloseButtonVisible() {
-      return this.hasCloseButton ? '' : 'is-hidden';
-    },
-    isLightTheme() {
-      return this.isLight ? 'is-light' : '';
-    },
-  },
-};
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  hasCloseButton: Boolean,
+  themeColor: String,
+  content: String,
+})
+
+const isCloseButtonVisible = computed(() => {
+  return props.hasCloseButton ? '' : 'is-hidden';
+})
+
 </script>
