d68c68ee0be441a6a0ecf7c40d3ee37a665f0468
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2025-01-31 16:51:48 +0800

    Add unit test for node (#183)

--- src/components/__tests__/QuestionBase.spec.js
+++ src/components/__tests__/QuestionBase.spec.js
@@ -10,11 +10,59 @@ const q = {
   shouldView: "Ask",
 };
 
-const valueSelector = "[data-test='does the person walk?']";
+const qq = {
+  andOr: {
+    tag: "All",
+    nl: {},
+    children: [
+      {
+        andOr: { tag: "Leaf", nl: {}, contents: "does the person walk?" },
+        mark: { value: "undefined", source: "user" },
+        prePost: {},
+        shouldView: "Ask",
+      },
+      {
+        andOr: {
+          tag: "Any",
+          nl: {},
+          children: [
+            {
+              andOr: { tag: "Leaf", nl: {}, contents: "does the person eat?" },
+              mark: { value: "undefined", source: "user" },
+              prePost: {},
+              shouldView: "Ask",
+            },
+            {
+              andOr: { tag: "Leaf", nl: {}, contents: "does the person drink?" },
+              mark: { value: "undefined", source: "user" },
+              prePost: {},
+              shouldView: "Ask",
+            },
+          ],
+        },
+        mark: { value: "undefined", source: "user" },
+        prePost: { Pre: "any of:" },
+        shouldView: "View",
+      },
+    ],
+  },
+  mark: { value: "undefined", source: "user" },
+  prePost: { Pre: "all of:" },
+  shouldView: "View",
+};
+
+const valueSelectorChild = "[data-test='does the person walk?']";
+const valueSelectorNode = "[data-test='all of:']";
+
 
 describe("Test QuestionBase Component", () => {
-  it("renders properly", () => {
+  it("renders leaf properly", () => {
     const wrapper = mount(QuestionBase, { props: { question: q } });
-    expect(wrapper.find(valueSelector).text()).toContain('Yes')
+    expect(wrapper.find(valueSelectorChild).text()).toContain("Yes");
+  });
+
+  it("renders tree node properly", () => {
+    const wrapper = mount(QuestionBase, { props: { question: qq } });
+    expect(wrapper.find(valueSelectorNode).text()).toContain("all of:");
   });
 });
