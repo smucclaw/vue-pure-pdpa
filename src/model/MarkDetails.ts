694074aa0337e09711faa7699969e8e769c916de
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2025-03-17 11:44:43 +0800

    Convert MarkDetails class to interface (#209)

--- src/model/MarkDetails.ts
+++ src/model/MarkDetails.ts
@@ -1,15 +1,13 @@
 import { Ternary, ternaryFromString } from "./Ternary";
 
-export class MarkDetails {
+export interface MarkDetails {
     source: string;
     value: Ternary;
-
-    constructor(source: string, value: Ternary) {
-        this.source = source;
-        this.value = value;
-    }
 }
 
 export function userMark(value: string): MarkDetails {
-    return new MarkDetails('user', ternaryFromString(value));
+    return {
+        source: 'user',
+        value: ternaryFromString(value)
+    };
 }
